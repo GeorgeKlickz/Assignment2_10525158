fc7c882b6c7ef8b93a200558dd1fb0cffef72279
GeorgeKlickz <georgeklickzkwakutipz@gmail.com>
2016-03-17 14:14:33 -0700

    Ten times

---
+++ TenTimes.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author GEORGE KLICKZ
+ */
+
+package assignment2_10525158;
+ 
+public class TenTimes {
+
+    public static void main(String[]args) 
+    {
+     for ( int n = 1 ; n <=10 ; n = n+1 )
+        {
+            System.out.println( n + ". " + "Mr. Mitchell is cool" );
+        }
+    }
+}
+
