4892dd009674dc9b532e99f31b4a11e5cf4ff70a
GeorgeKlickz <georgeklickzkwakutipz@gmail.com>
2016-03-17 14:01:03 -0700

    Keep Guessing

---
+++ KeepGuessing.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author GEORGE KLICKZ
+ */
+
+package assignment2_10525158;
+
+import java.util.Scanner;
+public class KeepGuessing {
+
+    public static void main(String[] args){
+    Scanner input = new Scanner (System.in);
+    int n=8;
+    int key;
+        System.out.printf("Im thinking if a number between 1 to 10.Try guessing it %n Your Guess>");
+        key=input.nextInt();
+            while(n!=key)
+            {
+                System.out.println("That is incorrect.Guess again");
+                key=input.nextInt();
+            }
+            System.out.printf("You are right,I was thinking of %d%n",n);
+}
+}
+
