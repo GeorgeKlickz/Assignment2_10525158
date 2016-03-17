fe1cfa69b4a1f7555be7d7cec339da96d617a99e
GeorgeKlickz <georgeklickzkwakutipz@gmail.com>
2016-03-17 14:16:33 -0700

    X and Y ... thank you very much Dr. Steve

---
+++ XandYs.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author GEORGE KLICKZ
+ */
+package assignment2_10525158;
+public class XandYs {
+    
+        public static void main(String[]arg)
+        {
+              double n;
+        for(n=-10.0; n<=10.0;n=n+0.5)
+        {
+           double y=n*n;
+            System.out.printf(n+"" );
+            
+            System.out.printf("%n %g" ,y );
+           
+        }
+        }
+    
+}
