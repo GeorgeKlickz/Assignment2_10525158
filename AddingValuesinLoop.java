903895e3176b2c11a6f5aae162cf7b94c89a1581
GeorgeKlickz <georgeklickzkwakutipz@gmail.com>
2016-03-17 12:55:57 -0700

    Adding Values in a loop

---
+++ AddingValuesinLoop.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author GEORGE KLICKZ
+ */
+package Assignment2_10525158;
+
+import java.util.Scanner;
+public class AddingValuesinLoop {
+    
+
+   
+    public static void main(String[]args)
+    {
+    Scanner input = new Scanner(System.in);
+            int n;
+            int j;
+                  
+            System.out.println("I will add the numbers you gave me");
+            n=input.nextInt();
+            System.out.println("Give me aother one");
+            while(n!=0)
+            {
+                 j=input.nextInt();
+                n=n+j;
+             if(j!=0)
+              {
+                  System.out.printf("The total so far is %d%n",n);
+                System.out.printf("Enter another number%n");
+              }
+              else if(j==0)
+              {
+              System.out.printf("The total so far is %d%n",n);
+                n=0;
+              }
+               
+            }
+            System.out.println("Please enter a non zero number");
+            
+    }
+  }
+
+
