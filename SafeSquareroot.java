21930cda551ee11723b7f3ac6c9957ad57e85bea
GeorgeKlickz <georgeklickzkwakutipz@gmail.com>
2016-03-17 14:13:41 -0700

    Safe Square root

---
+++ SafeSquareroot.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author GEORGE KLICKZ
+ */
+
+    package assignment2_10525158;
+
+
+import java.util.Scanner;
+public class SafeSquareroot { 
+
+    public static void main(String[]args)
+    {
+        Scanner input=new Scanner(System.in);
+        int n;
+        int r=0;
+        System.out.println("Enter the number for its Sqaure root");
+        n=input.nextInt();
+        while(n>0 || n<0)
+        {
+           if(n<0)
+           {
+               System.out.println("Try again you cant find the sqaure root of a negative number ");
+               n=input.nextInt();
+               
+           }
+           else
+           {
+           
+           Math.sqrt(n);
+           System.out.println("Its square root is "+ Math.sqrt(n));
+           break;
+           }
+     
+        }
+        System.out.println("You cant find the square root of a negative number ");
+    }
+}
+
+}
